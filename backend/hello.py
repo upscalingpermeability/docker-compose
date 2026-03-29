c697b556436df4f8169d71c3515b68c0a3b3c879
dssa-cti <ddsa@aeb.gov.br>
2026-03-29 16:15:03 -0300

    firs commit

---
+++ backend/hello.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.get("/usuarios")
+def listar():
+    return ["joão, maria"]
+
+@app.post("/criarusuarios")
+ def criar():
+      return  "criar"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
+
+
